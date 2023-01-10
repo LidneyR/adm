6f8784e0aaf9603cbefd3294f8cbdd199d6b2a6d
LidneyR <121246446+LidneyR@users.noreply.github.com>
2023-01-10 18:51:26 -0300

    Add files via upload

--- relatorio.js
+++ relatorio.js
@@ -5,15 +5,24 @@ function relatorio(){
 
 }
 
-
+ 
 function saved(){
  
-    // Store
-    localStorage.setItem("lastname", "Smith");
+    // Storexx  
+    allM=JSON.stringify(arrOrder)
 
+    console.log("entrou")
+ 
+    localStorage.setItem("oders",allM);
+  
+    if(arrOrder.length>0){
     
+        prodsSaved=JSON.parse(localStorage.getItem("oders"))
+        
+       
+    }   
 }
-
+ 
 
  
 function orderFortable(){
@@ -24,6 +33,7 @@ function orderFortable(){
         if(orMap.name===nMesa){ 
 
             orMap.orders.map((allOrTab)=>{
+
                 return allOrTab.idPedido
             })
         }
@@ -31,3 +41,27 @@ function orderFortable(){
     })
                     
 }
+
+
+function statusApp(){
+ 
+  prodSaved=JSON.parse(localStorage.getItem("oders"))
+  containerMesas=document.getElementById("mesas");
+
+  if(prodSaved.length>0){ 
+
+    arrOrder=prodSaved    
+   console.log(arrOrder)
+        arrOrder.map((mesaMap)=>{ 
+            
+            containerMesas.innerHTML+=`   
+                <button key='`+mesaMap.name+`' onclick="tabledetails(event)" class="mesabutton">`+mesaMap.name+`</button>
+            `; 
+        })
+
+        tableOptions()
+ 
+  }
+}
+
+statusApp()
